8cf3d72a681ca498da5f0dda4c57075b25077648
rejet <rejet@KAVYA>
2023-12-26 11:26:35 +0530

    Data intial commit

---
+++ src/main/java/com/ganesh/sarma/data/NakshatramDetails.java
@@ -0,0 +1,10 @@
+package com.ganesh.sarma.data;
+
+public class NakshatramDetails {
+	
+	String aswini1 = "అశ్విని,1,";
+	String aswini2 = "అశ్విని,2,";
+	String aswini3 = "";
+	String aswini4 = "";
+	
+}
