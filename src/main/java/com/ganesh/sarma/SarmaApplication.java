e5bcb1c6dc6d8090e5f077f0cf53aec46a8f5678
rejet <rejet@KAVYA>
2023-12-30 00:17:47 +0530

    made changes to open app in browser

--- src/main/java/com/ganesh/sarma/SarmaApplication.java
+++ src/main/java/com/ganesh/sarma/SarmaApplication.java
@@ -1,13 +1,20 @@
 package com.ganesh.sarma;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SarmaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SarmaApplication.class, args);
+		openHomePage();
 	}
 
+	private static void openHomePage() throws IOException {
+	       Runtime rt = Runtime.getRuntime();
+	       rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/nakshatram/ponthana");
+	    }
 }
